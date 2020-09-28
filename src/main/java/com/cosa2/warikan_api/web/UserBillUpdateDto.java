4ade96bce1aaa8e7675a4037d1f53bac574e61fd
hudifu316 <hudifu316@gmail.com>
2020-09-28 11:17:50 +0900

    UserBillUpdateDtoにpayAmmountフィールド追加

--- src/main/java/com/cosa2/warikan_api/web/UserBillUpdateDto.java
+++ src/main/java/com/cosa2/warikan_api/web/UserBillUpdateDto.java
@@ -1,5 +1,6 @@
 package com.cosa2.warikan_api.web;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import javax.persistence.Id;
@@ -12,10 +13,11 @@ public class UserBillUpdateDto {
 	public UserBillUpdateDto() {
 	}
 
-	public UserBillUpdateDto(UUID userId2, String username2, boolean kanji2) {
+	public UserBillUpdateDto(UUID userId2, String username2, boolean kanji2, BigDecimal payAmount2) {
 		this.userId = userId2;
 		this.username = username2;
 		this.kanji = kanji2;
+		this.payAmount = payAmount2;
 	}
 
 	@Id
@@ -25,5 +27,7 @@ public class UserBillUpdateDto {
 	private String username;
 
 	private boolean kanji;
+
+	private BigDecimal payAmount;
 
 }
